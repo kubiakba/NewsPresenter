53bc55714061d974dfe8a4876408c29b6980ed99
kubiakb <kubiakb1@gmail.com>
2018-10-13 11:39:16 +0200

    test parsing json to object

--- src/test/java/pl/bk/news/NewsApplicationTests.java
+++ src/test/java/pl/bk/news/NewsApplicationTests.java
@@ -1,14 +1,43 @@
 package pl.bk.news;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import pl.bk.news.external.news.dto.News;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.util.stream.Collectors.joining;
 
 public class NewsApplicationTests
 {
-    @Test
-    public void contextLoads() throws IOException
+    BufferedReader reader;
+    ObjectMapper mapper = new ObjectMapper();
+    
+    @Before
+    public void setup()
     {
+        reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/news.json")));
+    }
+    @After
+    public void close() throws IOException
+    {
+        reader.close();
+    }
+    
+    @Test
+    public void shouldParseJsonFromNewsApi() throws IOException
+    {
+        //given
+        final String json = reader.lines().collect(joining());
+        //when
+        final News newses = mapper.readValue(json, News.class);
+        //then
+        Assertions.assertThat(newses.getArticles().size()).isNotZero();
     }
     
 }
