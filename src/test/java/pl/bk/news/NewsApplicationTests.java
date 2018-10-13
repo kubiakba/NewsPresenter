0f2ffe18583ed085d62b49718286f199e7d33729
kubiakb <kubiakb1@gmail.com>
2018-10-13 11:23:01 +0200

    initial commit

---
+++ src/test/java/pl/bk/news/NewsApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.bk.news;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class NewsApplicationTests
+{
+    @Test
+    public void contextLoads() throws IOException
+    {
+    }
+    
+}
