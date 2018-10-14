7b1100d00f23c6f6010431cfd22595259890312f
kubiakba <kubiakb1@gmail.com>
2018-10-14 18:47:22 +0200

    refactor test

--- src/test/java/pl/bk/news/NewsApplicationTests.java
+++ src/test/java/pl/bk/news/NewsApplicationTests.java
@@ -1,7 +1,6 @@
 package pl.bk.news;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NewsApplicationTests
 {
@@ -23,6 +23,7 @@ public class NewsApplicationTests
     {
         reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/news.json")));
     }
+    
     @After
     public void close() throws IOException
     {
@@ -37,7 +38,15 @@ public class NewsApplicationTests
         //when
         final News newses = mapper.readValue(json, News.class);
         //then
-        Assertions.assertThat(newses.getArticles().size()).isNotZero();
+        assertThat(newses.getArticles().size()).isEqualTo(2);
+        assertThat(newses.getArticles().get(0).getSource().getName()).isEqualTo("TechCrunch");
+        assertThat(newses.getArticles().get(0).getAuthor()).isEqualTo("Joyce Yang");
+        assertThat(newses.getArticles().get(0).getTitle()).isEqualTo("The 21-day bitcoin challenge");
+        assertThat(newses.getArticles().get(0).getDescription()).isEqualTo(
+            "There is a documentary series currently airing on iQiyi, China’s Netflix equivalent, about a Chinese bitcoin enthusiast who attempts to survive 21 days by merely living on 0.21 bitcoin, or $1,300, without any help or donations. He You Bing is traveling and ca…");
+        assertThat(newses.getArticles().get(0).getUrl()).isEqualTo("http://techcrunch.com/2018/09/16/the-21-day-bitcoin-challenge/");
+        assertThat(newses.getArticles().get(0).getUrlToImage()).isEqualTo("https://techcrunch.com/wp-content/uploads/2018/09/Screen-Shot-2018-09-16-at-8.57.40-AM.png?w=745");
+        assertThat(newses.getArticles().get(0).getPublishedAt()).isEqualTo("2018-09-16T13:11:12Z");
     }
     
 }
