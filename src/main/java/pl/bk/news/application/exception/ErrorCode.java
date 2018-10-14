8766df560e90f6212eab87c72ec2a9370a5f49d6
kubiakba <kubiakb1@gmail.com>
2018-10-14 18:49:08 +0200

    exception handling

---
+++ src/main/java/pl/bk/news/application/exception/ErrorCode.java
@@ -0,0 +1,6 @@
+package pl.bk.news.application.exception;
+
+public enum ErrorCode
+{
+    INVALID_COUNTRY_NAME, INTERNAL_ERROR
+}
---
+++ src/main/java/pl/bk/news/application/exception/ErrorMessage.java
@@ -0,0 +1,14 @@
+package pl.bk.news.application.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ErrorMessage
+{
+    private ErrorCode errorCode;
+}
